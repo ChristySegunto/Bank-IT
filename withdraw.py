e09b64dac874a5c70ece06ad7f11cd1832adb89f
Christy Segunto <83587757+ChristySegunto@users.noreply.github.com>
2023-01-16 15:28:04 +0800

    Update withdraw.py

--- withdraw.py
+++ withdraw.py
@@ -7,16 +7,16 @@ from tkinter import messagebox
 withdraw = Tk()
 withdraw.geometry("1100x628")
 withdraw.title("Bank IT")
-withdraw.iconbitmap(r'C:\Users\bossg\OneDrive\Desktop\My Programs\python_programs\BANK IT\Images\logo.ico')
+withdraw.iconbitmap(r'image files\logo.ico')
 
 # window background
-withdraw_background = ImageTk.PhotoImage(file=r'C:\Users\bossg\OneDrive\Desktop\My Programs\python_programs\BANK IT\Images\backgroundtwo.png')
+withdraw_background = ImageTk.PhotoImage(file=r'image files\backgroundtwo.png')
 canvas1 = Canvas(withdraw, width=1100, height=628)
 canvas1.pack(fill = "both", expand = True)
 canvas1.create_image( 0, 0, image = withdraw_background, anchor = "nw")
 
 # window logo
-bankIT_logo = ImageTk.PhotoImage(file=r'C:\Users\bossg\OneDrive\Desktop\My Programs\python_programs\BANK IT\Images\logo.png')
+bankIT_logo = ImageTk.PhotoImage(file=r'image files\logo.png')
 canvas1.create_image(80, 40, image = bankIT_logo, anchor="nw")
 
 # window fonts
@@ -30,7 +30,7 @@ canvas1.create_text( 550, 240, text = "How much do you wish to withdraw?", fill=
 canvas1.create_text( 550, 260, text = "Enter your desired amount:", fill='white', font=subfont)
 
 # window textbox
-bluetextbox = Image.open(r'C:\Users\bossg\OneDrive\Desktop\My Programs\python_programs\BANK IT\Images\bluetextbox.png')
+bluetextbox = Image.open(r'image files\bluetextbox.png')
 bluetextbox_resize = bluetextbox.resize((370, 60))
 bluetextbox_final = ImageTk.PhotoImage(bluetextbox_resize)
 canvas1.create_image(370, 280, anchor=NW, image=bluetextbox_final)
@@ -44,7 +44,7 @@ bluetextbox_entry_layout = canvas1.create_window(550, 310, window=bluetextbox_en
 # money buttons
 def button1(event):
     print("1000")
-button1_image = Image.open(r'C:\Users\bossg\OneDrive\Desktop\My Programs\python_programs\BANK IT\Images\onethousand.png')
+button1_image = Image.open(r'image files\onethousand.png')
 button1_image_resize = button1_image.resize((1100,628))
 button1_image_final = ImageTk.PhotoImage(button1_image_resize)
 button1_image_layout = canvas1.create_image(0, 20, anchor=NW, image=button1_image_final)
@@ -60,7 +60,7 @@ canvas1.tag_bind(button2_image_layout, "<Button-1>", button2)
 
 def button3(event):
     print("5000")
-button3_image = Image.open(r'C:\Users\bossg\OneDrive\Desktop\My Programs\python_programs\BANK IT\Images\fivethousand.png')
+button3_image = Image.open(r'image files\fivethousand.png')
 button3_image_resize = button3_image.resize((1100,628))
 button3_image_final = ImageTk.PhotoImage(button3_image_resize)
 button3_image_layout = canvas1.create_image(0, 20, anchor=NW, image=button3_image_final)
@@ -68,7 +68,7 @@ canvas1.tag_bind(button3_image_layout, "<Button-1>", button3)
 
 def button4(event):
     print("10000")
-button4_image = Image.open(r'C:\Users\bossg\OneDrive\Desktop\My Programs\python_programs\BANK IT\Images\tenthousand.png')
+button4_image = Image.open(r'image files\tenthousand.png')
 button4_image_resize = button4_image.resize((1100,628))
 button4_image_final = ImageTk.PhotoImage(button4_image_resize)
 button4_image_layout = canvas1.create_image(0, 20, anchor=NW, image=button4_image_final)
@@ -77,7 +77,7 @@ canvas1.tag_bind(button4_image_layout, "<Button-1>", button4)
 # submit button
 def button5(event):
     print("submit")
-submitbutton_image = Image.open(r'C:\Users\bossg\OneDrive\Desktop\My Programs\python_programs\BANK IT\Images\submitbutton.png')
+submitbutton_image = Image.open(r'image files\submitbutton.png')
 submitbutton_image_resize = submitbutton_image.resize((1100,628))
 submitbutton_image_final = ImageTk.PhotoImage(submitbutton_image_resize)
 submitbutton_image_layout = canvas1.create_image(-40, 15, anchor=NW, image=submitbutton_image_final)
@@ -86,7 +86,7 @@ canvas1.tag_bind(submitbutton_image_layout, "<Button-1>", button5)
 # back button
 def backbutton(event):
     print("back")
-backbutton_image = Image.open(r'C:\Users\bossg\OneDrive\Desktop\My Programs\python_programs\BANK IT\Images\bluebackbutton.png')
+backbutton_image = Image.open(r'image files\bluebackbutton.png')
 backbutton_image_resize = backbutton_image.resize((1100,628))
 backbutton_image_final = ImageTk.PhotoImage(backbutton_image_resize)
 backbutton_image_layout = canvas1.create_image(0, 0, anchor=NW, image=backbutton_image_final)
